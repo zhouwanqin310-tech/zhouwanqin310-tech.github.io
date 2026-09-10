8c7d829e7f18f3679140cf11d8612fe5163df7c5
zhouwanqin310-tech <zhouwanqin310@gmail.com>
2026-09-10 14:18:52 +0800

    Delete src directory

--- src/content/content.11tydata.js
+++
@@ -1,8 +0,0 @@
-import { route, contentUpdated } from '../../lib/content.js';
-export default {
-  layout: 'detail.njk',
-  eleventyComputed: {
-    permalink: data => route(data),
-    contentUpdated: data => contentUpdated(data, data.page.inputPath)
-  }
-};
